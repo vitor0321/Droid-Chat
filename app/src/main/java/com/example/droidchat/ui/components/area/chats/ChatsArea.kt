b5958cdf1bf440085c81c3aea4574f730788c9f3
Vitor Walcker <vitor0321@gmail.com>
2025-02-15 14:43:18 -0300

    feat: implement chat item shimmer effect and update UI components

--- app/src/main/java/com/example/droidchat/ui/components/area/chats/ChatsArea.kt
+++ app/src/main/java/com/example/droidchat/ui/components/area/chats/ChatsArea.kt
@@ -1,16 +1,22 @@
 package com.example.droidchat.ui.components.area.chats
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.droidchat.domain.model.Chat
+import com.example.droidchat.ui.components.field.chats.ChatItemShimmer
 import com.example.droidchat.ui.components.field.chats.ContentBoxChatField
 import com.example.droidchat.ui.components.field.chats.SuccessField
 import com.example.droidchat.ui.feature.chats.viewModel.ChatsListUiState
 import com.example.droidchat.ui.preview.ChatListPreviewParameterProvider
 import com.example.droidchat.ui.theme.DroidChatTheme
+import com.example.droidchat.ui.theme.DroidSpace
+import com.example.droidchat.ui.theme.Grey1
 import kotlinx.collections.immutable.ImmutableList
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,7 +30,15 @@ internal fun ChatsArea(
     ) {
         when (state) {
             is ChatsListUiState.Loading -> {
-                // Loading
+                Column(
+                    modifier = Modifier.padding(DroidSpace.MMedium.value)
+                ) {
+                    repeat(8) { index ->
+                        ChatItemShimmer()
+                        if (index < 7)
+                            HorizontalDivider(color = Grey1)
+                    }
+                }
             }
 
             is ChatsListUiState.Success -> {
@@ -38,7 +52,7 @@ internal fun ChatsArea(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 private fun ChatsAreaPreview(
     @PreviewParameter(ChatListPreviewParameterProvider::class)
@@ -47,6 +61,26 @@ private fun ChatsAreaPreview(
     DroidChatTheme {
         ChatsArea(
             state = ChatsListUiState.Success(chats)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun ChatsAreaLoadingPreview() {
+    DroidChatTheme {
+        ChatsArea(
+            state = ChatsListUiState.Loading,
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun ChatsAreaErrorPreview() {
+    DroidChatTheme {
+        ChatsArea(
+            state = ChatsListUiState.Error,
         )
     }
 }
